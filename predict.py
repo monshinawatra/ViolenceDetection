4e0f4132511164880c754866e510b91d889a5656
monchinawat <monchinawat@gmail.com>
2022-06-21 13:42:17 +0700

    release 1.0.0

--- predict.py
+++ predict.py
@@ -10,12 +10,13 @@ import argparse
 from tqdm import tqdm
 from get_model import get_model
 
+
 def get_resized_img(img, 
                     width: int=224, height: int=224, 
                     normalized: bool=True, predicted: bool=True, 
                     model=None):
     resized_img = cv2.resize(img, (width, height))
-    
+
     if normalized: resized_img = resized_img.astype(np.float32)/255.
     if predicted: 
         prediction = model.predict(np.expand_dims(resized_img, 0), verbose=0)
@@ -23,58 +24,72 @@ def get_resized_img(img,
     return resized_img
 
 
-def pred_video(vid_path, image_model, lstm_model, write_csv: bool=True):
+def pred_video(vid_path, image_model, lstm_model):
     cap = cv2.VideoCapture(vid_path)
     total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+
     images = deque()
     class_index = 2
     prob = 0.
+
     violence_frequency = []
-    
+
     if args.write_vid != None:
         frame_width = int(cap.get(3))
         frame_height = int(cap.get(4))
+
+        r = os.path.dirname(args.write_vid)
+        if not os.path.exists(r):
+            os.makedirs(r)
+
         out = cv2.VideoWriter(args.write_vid, 
-                              cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame_width,frame_height))
-        
-        
+                              cv2.VideoWriter_fourcc('M','J','P','G'), 
+                              fps, (frame_width,frame_height))
+
+
     for no_frame in tqdm(range(total_frames)):
         ret, frame = cap.read()
         if not ret:
             break
-        
+
         images.append(get_resized_img(frame, model=image_model)[0])
 
-        
+
         if len(images) == SEQUENCE_LENGHT:
             seq = np.asarray(images).reshape(1, 20, 4096)
 
             pred = lstm_model.predict(seq, verbose=0)
             class_index = np.argmax(pred[0])
             prob = pred[0, class_index]
-            
+
             images.popleft() # Remove first frame
+            dict_prob = prob
             if class_index == 0:
-                prob = 1 - prob
+                dict_prob = 1 - dict_prob
                 
-            violence_data = {'no_frame': no_frame, 'violence': prob}
+            violence_data = {'no_frame': no_frame, 'violence': dict_prob}
             violence_frequency.append(violence_data)
         
         cv2.putText(frame, f"{CLASSES[class_index]} {'{:.2f}'.format(prob * 100)}%", (10, 30),
                     cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-        
+
         if args.write_vid != None: out.write(frame)
-        
+
     cap.release()
     if args.write_vid != None: out.release()
     cv2.destroyAllWindows()
 
     if args.write_csv != None:
         df = pd.DataFrame(violence_frequency).set_index('no_frame')
+
+        r = os.path.dirname(args.write_csv)
+        if not os.path.exists(r):
+            os.makedirs(r)
+
         if os.path.exists(args.write_csv):
             os.remove(args.write_csv)
-        
+
         df.to_csv(args.write_csv)
 
 
@@ -83,7 +98,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--dir', type=str, default='dataset/Test/Violence/NV_353.mp4', help='')
     parser.add_argument('--random', action='store_true')
-        
+
     parser.add_argument('--write-csv', type=str, help='')
     parser.add_argument('--write-vid', type=str, help='')
     args = parser.parse_args()
@@ -113,8 +128,8 @@ if __name__ == "__main__":
     else:
         selected_video = args.dir
 
-    os.system('cls||clear')
 
+    os.system('cls||clear')
 
     print('Processing..')
     print('- directory', args.dir)
@@ -127,4 +142,3 @@ if __name__ == "__main__":
         args.write_csv = args.write_csv.replace("'", '').replace('"', '')
     
     pred_video(selected_video, image_model, model_)
-    
